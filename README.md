66082aef6e7120728a2caa28e00bb8ed420dbb11
infinitnet <co@infinitnet.io>
2023-12-27 13:46:06 +0100

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# relevant-recent-posts
+# Relevant Recent Posts
 Adds WordPress shortcodes for recent posts lists.
+
+## Purpose
+The Relevant Recent Posts plugin enhances your WordPress site by providing a simple yet flexible way to display lists of recent posts. It's particularly useful for bloggers and content-focused websites, where showcasing related or recent content can improve SEO, user engagement, and site navigation.
+
+## Installation
+1. Download the latest release.
+2. Upload and install the plugin zip file under Plugins > Add New Plugin > Upload Plugin.
+3. Activate the plugin.
+
+## Available Shortcodes
+- **[recentposts]**: Displays the 10 most recent posts from the same category as the current post. 
+- **[recentposts count=5]**: Changes the number of displayed posts. Replace `5` with your desired number.
+- **[recentposts scope=global]**: Displays recent posts from all categories.
+- **[recentposts date=modified]**: Sorts posts by the last modified date instead of the published date.
+- **[recentposts class=example-class]**: Applies a custom CSS class (`example-class`) to the list for styling. Replace `example-class` with your own class name.
+- **[recentposts nonav]**: Removes `<nav>` tags from the posts list.
+- You can combine multiple parameters, like `[recentposts date=modified scope=global count=5 class=example-class]`.
+
+## Usage
+Add the shortcode `[recentposts]` to any post, page, or widget where you want to display the list of recent posts. Customize its behavior using the available parameters.
+
+## Support
+For support, feature requests, or bug reports, please visit the [GitHub issues page](https://github.com/infinitnet/relevant-recent-posts/issues).
+
+## Contributions
+Contributions to the plugin are welcome. Please fork the GitHub repository and submit a pull request.
+
+## Changelog
+- **1.0**: Initial release.
---
+++ relevant-recent-posts.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Plugin Name: Relevant Recent Posts
+ * Description: Adds shortcodes for recent posts lists.
+ * Author: Infinitnet
+ * Author URI: https://infinitnet.io/
+ * Plugin URI: https://github.com/infinitnet/relevant-recent-posts
+ * Update URI: https://github.com/infinitnet/relevant-recent-posts
+ * Version: 1.0
+ * License: GPLv3
+ * Text Domain: relevant-recent-posts
+ */
+
+if ( ! function_exists( 'infinitnet_relevant_recent_posts_shortcode' ) ) {
+
+    function infinitnet_relevant_recent_posts_shortcode($atts) {
+        global $post;
+
+        $atts = shortcode_atts(array(
+            'count' => 10,
+            'scope' => 'category',
+            'date'  => 'published',
+            'class' => '',
+            'nonav' => false
+        ), $atts, 'recentposts');
+
+        $count = intval($atts['count']);
+        $count = max(1, min($count, 200));
+
+        $valid_scopes = ['category', 'global'];
+        if (!in_array($atts['scope'], $valid_scopes)) {
+            $atts['scope'] = 'category';
+        }
+
+        $valid_dates = ['published', 'modified'];
+        if (!in_array($atts['date'], $valid_dates)) {
+            $atts['date'] = 'published';
+        }
+
+        if (!isset($post) || $post == null) {
+            return '<p>' . esc_html__('Recent posts cannot be displayed.', 'relevant-recent-posts') . '</p>';
+        }
+
+        $args = array(
+            'posts_per_page' => $count,
+            'orderby' => ($atts['date'] === 'modified') ? 'modified' : 'date',
+            'order'   => 'DESC'
+        );
+
+        if ($atts['scope'] === 'category') {
+            $categories = get_the_category($post->ID);
+            if (!empty($categories)) {
+                $category_ids = array_map(function($category) {
+                    return $category->term_id;
+                }, $categories);
+                $args['category__in'] = $category_ids;
+            }
+        }
+
+        $post_id = isset($post) ? $post->ID : 0;
+        $scope = $atts['scope'];
+        $date = $atts['date'];
+
+        $cache_key = "rrp_{$post_id}_{$scope}_{$date}";
+
+        $output = get_transient($cache_key);
+
+        if ($output === false) {
+            $recent_posts_query = new WP_Query($args);
+    
+            if ($recent_posts_query->have_posts()) {
+                $class_attr = !empty($atts['class']) ? ' class="' . esc_attr($atts['class']) . '"' : '';
+    
+                $output = $atts['nonav'] ? '' : '<nav>';
+                $output .= '<ul' . $class_attr . '>';
+    
+                while ($recent_posts_query->have_posts()) {
+                    $recent_posts_query->the_post();
+                    $output .= '<li><a href="' . esc_url(get_permalink()) . '">' . esc_html(get_the_title()) . '</a></li>';
+                }
+    
+                $output .= '</ul>';
+                $output .= $atts['nonav'] ? '' : '</nav>';
+    
+                set_transient($cache_key, $output, 12 * HOUR_IN_SECONDS);
+            } else {
+                $output = $atts['nonav'] ? '<ul><p>' : '<nav><ul><p>';
+                $output .= esc_html__('No posts found.', 'relevant-recent-posts');
+                $output .= $atts['nonav'] ? '</p></ul>' : '</p></ul></nav>';
+            }
+    
+            wp_reset_postdata();
+        }
+    
+        return $output;
+    }
+
+    add_shortcode('recentposts', 'infinitnet_relevant_recent_posts_shortcode');
+}
+
+function infinitnet_clear_recent_relevant_posts_cache($post_id) {
+    global $wpdb;
+
+    $wpdb->query("DELETE FROM $wpdb->options WHERE option_name LIKE '%rrp_{$post_id}_%'");
+}
+
+add_action('save_post', 'infinitnet_clear_recent_relevant_posts_cache');
+add_action('delete_post', 'infinitnet_clear_recent_relevant_posts_cache');
+add_action('transition_post_status', 'infinitnet_clear_recent_relevant_posts_cache', 10, 3);
